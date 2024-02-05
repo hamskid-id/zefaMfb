27fa39058f77354c4ffd4d70548b1ee5405651de
hamskid-id <lawalhamzat27@gmail.com>
2024-02-05 06:29:13 -0800

    updated

--- src/components/loan/sectionTwo.jsx
+++ src/components/loan/sectionTwo.jsx
@@ -1,13 +1,17 @@
 import { useForm } from "react-hook-form";
 import { Text } from "../text"
+import { InputField } from "../formField";
+import { AiFillDislike, AiFillLike } from "react-icons/ai";
 
 export const LoanSectionTwo =()=>{
     const{
         register,
+        watch,
         handleSubmit,
         formState: {errors}
     }=useForm();
-    
+    const loan_type = watch("loan_type")
+    const about_us = watch("about_us")
     return(
         <div className="my-14 bg-white py-14 px-4 xl:w-[73%] lg:w-[73%] md:w-[73%] sm:w-100%] xs:w-[100%] flex flex-col justify-center items-center m-auto">
             <Text
@@ -25,7 +29,7 @@ export const LoanSectionTwo =()=>{
             <div className="w-full bg-white rounded-md p-4 my-4">
                 <form target="_blank" action="https://formsubmit.co/webcontact@zefamfb.com" method="POST">
                     <div className="grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 xs:grid-cols-1 xxs:grid-cols-1 gap-4">
-                    <input type="hidden" name="_next" value="https://zefa.vercel.app/acct-opening-thanks.html"/>
+                    <input type="hidden" name="_next" value="https://zefa-microfinace.vercel.app/thank-you"/>
                     <input type="hidden" name="_subject" value="Loan Application Form Submission"/>
                     {
                         [
@@ -68,28 +72,19 @@ export const LoanSectionTwo =()=>{
                                 label
                             }=info;
                             return(
-                                <div 
-                                    className="lg:mb-3 xl:mb-3 md:mb-3 sm:mb-0 xs:mb-0 flex flex-col justify-start"
+                                <InputField 
+                                    labelTitle={title}
                                     key={index}
-                                >
-                                    <label
-                                        htmlFor={title}
-                                        className="pointer-events-none max-w-[90%] origin-[0_0] truncate leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-black peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-black"
-                                        >{label}
-                                    </label>
-                                    <input
-                                        type={type}
-                                        required
-                                        className="peer block text-black  w-full rounded border bg-transparent px-3 py-[0.37rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-black data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-black [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                        id={title}
-                                        name={title}
-                                        placeholder={holder} 
-                                    />
-                                </div>
+                                    inputType={type}
+                                    labelName={label}
+                                    watch={watch}
+                                    register={register}
+                                    placeholder={holder}
+                                />
                             )
                         })
                     }
-                    <div className="flex flex-col">
+                    <div className="flex flex-col relative">
                         <label
                             htmlFor="loan_type"
                             className="pointer-events-none max-w-[90%] origin-[0_0] truncate leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -98,6 +93,7 @@ export const LoanSectionTwo =()=>{
                         <select
                             name="loan_type" 
                             id="loan_type"
+                            {...register("loan_type",{required:true})}
                             className="lg:mb-3 xl:mb-3 md:mb-3 sm:mb-0 xs:mb-0 peer block text-black w-full rounded border bg-transparent px-3 py-[0.37rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                         >
                             <option value="" selected>Choose...</option>
@@ -113,8 +109,11 @@ export const LoanSectionTwo =()=>{
                             <option value="group-loan">Group Loan</option>
                             <option value="others">Others</option>
                         </select>
+                        <div className="absolute top-[2rem] right-[1.5rem]">
+                            { loan_type ? <AiFillLike size="1rem" color="#a7f3d0"/>:<AiFillDislike size="1rem" color="#fecdd3"/>}
+                        </div>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col relative">
                         <label
                             htmlFor="about_us"
                             className="pointer-events-none max-w-[90%] origin-[0_0] truncate leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -123,6 +122,7 @@ export const LoanSectionTwo =()=>{
                         <select  
                             name="about_us" 
                             id="about_us"
+                            {...register("about_us",{required:true})}
                             className="lg:mb-3 xl:mb-3 md:mb-3 sm:mb-0 xs:mb-0 peer block text-black w-full rounded border bg-transparent px-3 py-[0.37rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                         >
                             <option value="" selected="">Choose...</option>
@@ -136,6 +136,9 @@ export const LoanSectionTwo =()=>{
                             <option value="accountant">Accountant</option>
                             <option value="friend">Friend</option>
                         </select>
+                        <div className="absolute top-[2rem] right-[1.5rem]">
+                            { about_us ? <AiFillLike size="1rem" color="#a7f3d0"/>:<AiFillDislike size="1rem" color="#fecdd3"/>}
+                        </div>
                     </div>
                     </div>
                     <div>
